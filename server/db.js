69b68210b1b3c5f21908a9f76f0ae458b74f996b
Despampanante <santiago@depascale.org>
2023-02-04 23:25:51 -0500

    b

--- server/db.js
+++ server/db.js
@@ -1,11 +1,15 @@
 const Pool = require("pg").Pool;
 
+const connectionString = "postgresql://postgres:tF85ELcPfmwz0KIBC11s@containers-us-west-157.railway.app:6656/railway"
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "tF85ELcPfmwz0KIBC11s",
+//     host: "containers-us-west-157.railway.app",
+//     port: 6656,
+//     database: "railway"
+// });
 const pool = new Pool({
-    user: "postgres",
-    password: "tF85ELcPfmwz0KIBC11s",
-    host: "containers-us-west-157.railway.app",
-    port: 6656,
-    database: "railway"
-});
+    connectionString,
+})
 
 module.exports = pool;
