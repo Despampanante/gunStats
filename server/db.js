c147ef141774caf012083913d43a31edf641d929
Despampanante <santiago@depascale.org>
2023-02-04 22:17:12 -0500

    fixed pq hopefully

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
+    password: "tF85ELcPfmwz0KIBC11s",
+    host: "containers-us-west-157.railway.app",
+    port: 6656,
+    database: "railway"
 });
 
 module.exports = pool;
